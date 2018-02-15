2e66a62963ed48ed04c85649ab0c3a822671277e
Your Name <mdoran@oswego.edu>
2018-02-15 14:51:39 -0500

    adding libgdx project

---
+++ emacsProjects/CSC380/csc380parkingBuddy/project/core/src/com/mygdx/game/states/ThankYouState.java
@@ -0,0 +1,39 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.states.GuiStateManager;
+import com.mygdx.game.states.State;
+import com.mygdx.pb.ParkingBuddy;
+
+/**
+ * @author Mike
+ */
+public class ThankYouState extends State{
+    
+    private Texture background;
+    
+    public ThankYouState(GuiStateManager gsm) {
+        super(gsm);
+        background = new Texture("D:\\Cygwin\\home\\Mike\\emacsProjects\\CSC380\\csc380parkingBuddy\\project\\core\\assets\\Thanks.png");
+    }
+
+    @Override
+    protected void handleInput() {
+        if(Gdx.input.justTouched()) System.exit(1);
+    }
+
+    @Override
+    public void update(double dt) {
+        handleInput();
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0, 0, ParkingBuddy.width, ParkingBuddy.height);
+        sb.end();
+    }
+    
+}
