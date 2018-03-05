c1795b045f0884c5d54f4187b4406d35be393f60
Your Name <mdoran@oswego.edu>
2018-03-05 16:13:14 -0500

    Test

---
+++ libGDXproject/desktop/src/com/mygdx/parkingbuddy/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.mygdx.parkingbuddy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.parkingbuddy.PBApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+                //----Code that makes the height, width, and title variables work-----------
+                config.width = PBApp.width;
+                config.height = PBApp.height;
+                config.title = PBApp.title;
+                //--------------------------------------------------------------------------
+		new LwjglApplication(new PBApp(), config);
+	}
+}
