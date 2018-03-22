88b36afb97705fc5394d2ce158306b0a7a1705a1
Emma Brunell <ebrunell15@gmail.com>
2018-03-22 14:50:02 -0400

    Rendering ShinemanMap

--- pbapp/core/src/main/java/com/pbapp/core/ShinemanMapState.java
+++ pbapp/core/src/main/java/com/pbapp/core/ShinemanMapState.java
@@ -3,6 +3,7 @@ package com.pbapp.core;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.pbapp.core.PBApp;
 
 /**
@@ -12,15 +13,26 @@ import com.pbapp.core.PBApp;
 public class ShinemanMapState extends State {
     
     private Texture background;
+    private MapSprite map;
     
     public ShinemanMapState(GuiStateManager gsm) {
         super(gsm);
         background = new Texture("ShinemanMap.png");
+        map = new MapSprite(0,PBApp.height,"ShinemanLotEditedMap.png",-200,-950,-190,-336);
     }
 
     @Override
     protected void handleInput() {
+        
+        if(Gdx.input.isTouched()){
+            int deltaX = Gdx.input.getDeltaX();
+            int deltaY = Gdx.input.getDeltaY();
+            map.update(new Vector2(deltaX, deltaY));
+        }
+        
         if (Gdx.input.justTouched()) {
+            System.out.println("X: " + Gdx.input.getX());
+            System.out.println("y: " + Gdx.input.getY());
             
             boolean withinBackX = (Gdx.input.getX() >= 18) && (Gdx.input.getX() <= 65);
             boolean withinBackY = (Gdx.input.getY() >= 15) && (Gdx.input.getY() <= 68);
@@ -54,6 +66,7 @@ public class ShinemanMapState extends State {
     @Override
     public void render(SpriteBatch sb) {
         sb.begin();
+        sb.draw(map.getTexture(), map.getXpos(), map.getYpos(),1650,1200);
         sb.draw(background, 0, 0, PBApp.width, PBApp.height);
         sb.end();
     }
@@ -61,6 +74,7 @@ public class ShinemanMapState extends State {
     public void dispose() {
         //remember to add all drawn objects to this method.
         background.dispose();
+        map.getTexture().dispose();
     }
     
 }
