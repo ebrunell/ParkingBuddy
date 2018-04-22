83a666f7be7951f44d1736eb381c19fbc9db42e1
Emma Brunell <ebrunell15@gmail.com>
2018-04-21 22:07:55 -0400

    Added timer popup & functionality for SheldonMap

--- pbapp/core/src/main/java/com/pbapp/core/SheldonMapState.java
+++ pbapp/core/src/main/java/com/pbapp/core/SheldonMapState.java
@@ -17,6 +17,10 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.TemporalAdjusters;
+import static java.time.temporal.ChronoUnit.HOURS;
 
 /**
  * @author emmabrunell
@@ -25,12 +29,26 @@ public class SheldonMapState extends State {
 
     private Texture background;
     private Texture popUpBackground;
+    private Texture timerBackground;
     private Button backButton;
     private Button dataButton;
     private Button emptySpaceButton;
     private Button fillSpaceButton;
     private Button obstructedButton;
     private Button setTimerButton;
+    private Button sevenAMButton;
+    private Button eightAMButton;
+    private Button nineAMButton;
+    private Button tenAMButton;
+    private Button elevenAMButton;
+    private Button twelvePMButton;
+    private Button onePMButton;
+    private Button twoPMButton;
+    private Button threePMButton;
+    private Button fourPMButton;
+    private Button fivePMButton;
+    private Button sixPMButton;
+    private Button closeButton;
     private MapSprite map;
     private ParkingSpaceButton[] spaces;
     private ShapeRenderer sr;
@@ -44,22 +62,40 @@ public class SheldonMapState extends State {
     String host;
     String lotName;
     public boolean spacePressed;
+    public boolean timerPressed;
     public String spaceId;
 
     public SheldonMapState(GuiStateManager gsm) {
         super(gsm);
         background = new Texture("SheldonMap.png");
         popUpBackground = new Texture("popUp.png");
+        timerBackground = new Texture("timerPopUp.png");
         backButton = new Button("BackButton.png", new Vector2(37, 60), new Vector2(230, 50));
         dataButton = new Button("DataButton.png", new Vector2(400, 60), new Vector2(230, 50));
         emptySpaceButton = new Button("emptySpace.png", new Vector2(75, 405), new Vector2(100, 100));
         fillSpaceButton = new Button("fillSpace.png", new Vector2(275, 405), new Vector2(100, 100));
         obstructedButton = new Button("obstructed.png", new Vector2(75, 475), new Vector2(100, 100));
         setTimerButton = new Button("setTimer.png", new Vector2(275, 475), new Vector2(100, 130));
+
+        sevenAMButton = new Button("7AM.png", new Vector2(65, 375), new Vector2(100, 100));
+        eightAMButton = new Button("8AM.png", new Vector2(160, 375), new Vector2(100, 100));
+        nineAMButton = new Button("9AM.png", new Vector2(250, 375), new Vector2(100, 100));
+        tenAMButton = new Button("10AM.png", new Vector2(340, 375), new Vector2(100, 100));
+        elevenAMButton = new Button("11AM.png", new Vector2(65, 425), new Vector2(100, 100));
+        twelvePMButton = new Button("12PM.png", new Vector2(160, 425), new Vector2(100, 100));
+        onePMButton = new Button("1PM.png", new Vector2(250, 425), new Vector2(100, 100));
+        twoPMButton = new Button("2PM.png", new Vector2(340, 425), new Vector2(100, 100));
+        threePMButton = new Button("3PM.png", new Vector2(65, 475), new Vector2(100, 100));
+        fourPMButton = new Button("4PM.png", new Vector2(160, 475), new Vector2(100, 100));
+        fivePMButton = new Button("5PM.png", new Vector2(250, 475), new Vector2(100, 100));
+        sixPMButton = new Button("6PM.png", new Vector2(340, 475), new Vector2(100, 100));
+        closeButton = new Button("xButton.png", new Vector2(50, 342), new Vector2(100, 100));
+
         map = new MapSprite(0, PBApp.height, "SheldonLotEditedMap.png", 0, -700, 0, 0);
         spaces = new ParkingSpaceButton[67];
         sr = new ShapeRenderer();
         spacePressed = false;
+        timerPressed = false;
         port = "2525";
         lotName = "Sheldon";
         messageRecieved = false;
@@ -164,15 +200,15 @@ public class SheldonMapState extends State {
             System.out.println("X: " + x);
             //System.out.println("Xmap: " + map.getXpos());
             //System.out.println("deltaX: " + deltaX); 
-            
+
             //add condition somehow so that when map has reached left and right
             //boundaries, psb don't update anymore until not at boundaries
             map.update(new Vector2(deltaX, deltaY));
-            for (ParkingSpaceButton psb: spaces) {
+            for (ParkingSpaceButton psb : spaces) {
                 if (map.getXpos() > -700 && map.getXpos() < 0) {
-                    psb.update(deltaX,0);
+                    psb.update(deltaX, 0);
                 } else {
-                   //psb.update(0, 0);
+                    //psb.update(0, 0);
                 }
             }
         }
@@ -255,11 +291,179 @@ public class SheldonMapState extends State {
                         }
                     }
                     spacePressed = false;
+
                 } else if (setTimerButton.wasTouched(Gdx.input.getX(), Gdx.input.getY())) {
-                    if (messageRecieved) {
-                        System.out.println("Set Timer");
-                    }
+                    System.out.println("timer");
+                    //spacePressed = false;
+                    timerPressed = true;
+               
+
+                } else if (closeButton.wasTouched(Gdx.input.getX(), Gdx.input.getY())) {
+                    System.out.println("close button touched");
                     spacePressed = false;
+                    timerPressed = false;
+                }
+
+            }
+            
+            System.out.println(timerPressed);
+
+            if (timerPressed) {
+                System.out.println("TIMERPRESSED");
+                if (sevenAMButton.wasTouched(Gdx.input.getX(), Gdx.input.getY())) {
+                    LocalTime sevenAM = LocalTime.of(7, 0, 0);
+                    int time = (int)HOURS.between(sevenAM, LocalDateTime.now());
+                    if (messageRecieved) {
+                        try {
+                            o.writeObject(spaceId + " " + lotName + " fil " + time);
+                            o.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(SheldonMapState.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+
+                } else if (eightAMButton.wasTouched(Gdx.input.getX(), Gdx.input.getY())) {
+                    LocalTime eightAM = LocalTime.of(8, 0, 0);
+                    int time = (int)HOURS.between(eightAM, LocalDateTime.now());
+                    if (messageRecieved) {
+                        try {
+                            o.writeObject(spaceId + " " + lotName + " fil " + time);
+                            o.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(SheldonMapState.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    
+
+                } else if (nineAMButton.wasTouched(Gdx.input.getX(), Gdx.input.getY())) {
+                    LocalTime nineAM = LocalTime.of(9, 0, 0);
+                    int time = (int)HOURS.between(nineAM, LocalDateTime.now());
+                    if (messageRecieved) {
+                        try {
+                            o.writeObject(spaceId + " " + lotName + " fil " + time);
+                            o.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(SheldonMapState.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    
+
+                } else if (tenAMButton.wasTouched(Gdx.input.getX(), Gdx.input.getY())) {
+                    LocalTime tenAM = LocalTime.of(10, 0, 0);
+                    int time = (int)HOURS.between(tenAM, LocalDateTime.now());
+                    if (messageRecieved) {
+                        try {
+                            o.writeObject(spaceId + " " + lotName + " fil " + time);
+                            o.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(SheldonMapState.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+
+                } else if (elevenAMButton.wasTouched(Gdx.input.getX(), Gdx.input.getY())) {
+                    LocalTime elevenAM = LocalTime.of(11, 0, 0);
+                    int time = (int)HOURS.between(elevenAM, LocalDateTime.now());
+                    if (messageRecieved) {
+                        try {
+                            o.writeObject(spaceId + " " + lotName + " fil " + time);
+                            o.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(SheldonMapState.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    
+
+                } else if (twelvePMButton.wasTouched(Gdx.input.getX(), Gdx.input.getY())) {
+                    LocalTime twelvePM = LocalTime.of(12, 0, 0);
+                    int time = (int)HOURS.between(twelvePM, LocalDateTime.now());
+                    if (messageRecieved) {
+                        try {
+                            o.writeObject(spaceId + " " + lotName + " fil " + time);
+                            o.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(SheldonMapState.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    
+
+                } else if ((onePMButton.wasTouched(Gdx.input.getX(), Gdx.input.getY()))) {
+                    LocalTime onePM = LocalTime.of(13, 0, 0);
+                    int time = (int)HOURS.between(onePM, LocalDateTime.now());
+                    if (messageRecieved) {
+                        try {
+                            o.writeObject(spaceId + " " + lotName + " fil " + time);
+                            o.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(SheldonMapState.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    
+
+                } else if (twoPMButton.wasTouched(Gdx.input.getX(), Gdx.input.getY())) {
+                    LocalTime twoPM = LocalTime.of(14, 0, 0);
+                    int time = (int)HOURS.between(twoPM, LocalDateTime.now());
+                    if (messageRecieved) {
+                        try {
+                            o.writeObject(spaceId + " " + lotName + " fil " + time);
+                            o.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(SheldonMapState.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    
+
+                } else if (threePMButton.wasTouched(Gdx.input.getX(), Gdx.input.getY())) {
+                    LocalTime threePM = LocalTime.of(15, 0, 0);
+                    int time = (int)HOURS.between(threePM, LocalDateTime.now());
+                    if (messageRecieved) {
+                        try {
+                            o.writeObject(spaceId + " " + lotName + " fil " + time);
+                            o.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(SheldonMapState.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    
+
+                } else if (fourPMButton.wasTouched(Gdx.input.getX(), Gdx.input.getY())) {
+                    LocalTime fourPM = LocalTime.of(16, 0, 0);
+                    int time = (int)HOURS.between(fourPM, LocalDateTime.now());
+                    if (messageRecieved) {
+                        try {
+                            o.writeObject(spaceId + " " + lotName + " fil " + time);
+                            o.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(SheldonMapState.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    
+
+                } else if (fivePMButton.wasTouched(Gdx.input.getX(), Gdx.input.getY())) {
+                    LocalTime fivePM = LocalTime.of(17, 0, 0);
+                    int time = (int)HOURS.between(fivePM, LocalDateTime.now());
+                    if (messageRecieved) {
+                        try {
+                            o.writeObject(spaceId + " " + lotName + " fil " + time);
+                            o.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(SheldonMapState.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    
+
+                } else if (sixPMButton.wasTouched(Gdx.input.getX(), Gdx.input.getY())) {
+                    LocalTime sixPM = LocalTime.of(18, 0, 0);
+                    int time = (int)HOURS.between(sixPM,LocalDateTime.now());
+                    if (messageRecieved) {
+                        try {
+                            o.writeObject(spaceId + " " + lotName + " fil " + time);
+                            o.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(SheldonMapState.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    
+                    
                 }
 
             }
@@ -301,6 +505,36 @@ public class SheldonMapState extends State {
             sb.draw(setTimerButton.getTexture(), setTimerButton.getXpos(), setTimerButton.getYpos());
             sb.end();
 
+            sb.begin();
+            sb.draw(closeButton.getTexture(), closeButton.getXpos(), closeButton.getYpos());
+            sb.end();
+
+        }
+
+        if (timerPressed) {
+            System.out.println("Timer RENDER");
+            sb.begin();
+            sb.draw(timerBackground, 50, 300);
+            sb.end();
+
+            sb.begin();
+            sb.draw(sevenAMButton.getTexture(), sevenAMButton.getXpos(), sevenAMButton.getYpos());
+            sb.draw(eightAMButton.getTexture(), eightAMButton.getXpos(), eightAMButton.getYpos());
+            sb.draw(nineAMButton.getTexture(), nineAMButton.getXpos(), nineAMButton.getYpos());
+            sb.draw(tenAMButton.getTexture(), tenAMButton.getXpos(), tenAMButton.getYpos());
+            sb.draw(elevenAMButton.getTexture(), elevenAMButton.getXpos(), elevenAMButton.getYpos());
+            sb.draw(twelvePMButton.getTexture(), twelvePMButton.getXpos(), twelvePMButton.getYpos());
+            sb.draw(onePMButton.getTexture(), onePMButton.getXpos(), onePMButton.getYpos());
+            sb.draw(twoPMButton.getTexture(), twoPMButton.getXpos(), twoPMButton.getYpos());
+            sb.draw(threePMButton.getTexture(), threePMButton.getXpos(), threePMButton.getYpos());
+            sb.draw(fourPMButton.getTexture(), fourPMButton.getXpos(), fourPMButton.getYpos());
+            sb.draw(fivePMButton.getTexture(), fivePMButton.getXpos(), fivePMButton.getYpos());
+            sb.draw(sixPMButton.getTexture(), sixPMButton.getXpos(), sixPMButton.getYpos());
+            sb.end();
+
+            sb.begin();
+            sb.draw(closeButton.getTexture(), closeButton.getXpos(), closeButton.getYpos());
+            sb.end();
         }
     }
 
